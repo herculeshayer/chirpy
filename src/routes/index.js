420940f3f48499dd8eb5501a360909e31bd1d25a
Harman Hayer <hhhayer@gmail.com>
2021-09-15 21:52:15 -0700

    require and export api login/register endpoints

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,7 @@
-const 
+const userLogin = require('./userLogin');
+const userRegister = require('./userRegister');
+
+module.exports = app => {
+    app.use('/api/login', userLogin);
+    app.use('/api/register', userRegister);
+}
