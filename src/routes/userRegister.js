3076ba8f058f06598bc49229f44c99be61206e63
Harman Hayer <hhhayer@gmail.com>
2021-09-17 15:01:07 -0700

    post and get requests for user registration succes

--- src/routes/userRegister.js
+++ src/routes/userRegister.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', ( req , res ) => {
-    res.send('userRegister');
+const User = require('../models/users');
+
+router.get('/', async ( req , res ) => {
+    // console.log(users.findById(req.params.id));
+    try {
+        const userList = await User.find();
+        res.status(200).json(userList);
+    } catch (error) {
+        throw error;
+    }
+    // res.send('userRegister');
+})
+
+router.post('/', ( req, res ) => {
+    const { username, email, password } = req.body;
+    const newUser = new User({
+        username: username,
+        email: email,
+        password: password
+    })
+    try {
+        const addNewUser = newUser.save();
+        res.status(201).json(addNewUser);
+    } catch (error) {
+        res.status(400).json({message: error});
+        throw error;
+    }
 })
 
 module.exports = router;
