932e13906ffeaab30fa0d2a52337135df24d4c26
Harman Hayer <hhhayer@gmail.com>
2021-09-20 20:41:09 -0700

    add basic template home and logout routes

---
+++ src/routes/userLogout.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const router = express.Router();
+
+const redirectLogin = (req, res, next) => {
+    if(!req.session.userID) {
+        res.redirect('/login')
+    } else {
+        next();
+    }
+}
+
+router.get('/', redirectLogin, (req, res) => {
+    res.send('userlogout');
+})
+router.post('/', async (req, res) => {
+    
+})
+
+module.exports = router;
