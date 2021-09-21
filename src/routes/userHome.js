15ab9ab5c631dee930aa5a7d73586c970071a8d7
Harman Hayer <hhhayer@gmail.com>
2021-09-21 15:25:51 -0700

    add small html to visualize login

--- src/routes/userHome.js
+++ src/routes/userHome.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+
+const User = require('./../models/users');
+
 
 const router = express.Router();
 
-const redirectLogin = (req, res, next) => {
-    if(!req.session.userID) {
-        res.redirect('/login')
-    } else {
-        next();
-    }
-}
 
-router.get('/', redirectLogin, (req, res) => {
-    res.send('home');
+
+router.get('/', async (req, res) => {
+    try {
+        const user = await User.findOne({ _id: req.session.userID });
+        // console.log(req.session)
+        // console.log(user);
+
+        res.send(`
+        <h1>Home</h1>
+        <ul>
+            <li>Name: ${user.username}</li>
+            <li>Email: ${user.email}</li>
+        </ul>
+        `)
+    } catch (error) {
+        res.status(500).json({message: error });
+    }
+
+    
 })
 router.post('/', async (req, res) => {
     
