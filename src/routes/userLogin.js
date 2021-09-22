ea39a8750095d0d53872bf9c8961f92800389a94
Harman Hayer <hhhayer@gmail.com>
2021-09-21 20:35:28 -0700

    remove commented out code, add details for code

--- src/routes/userLogin.js
+++ src/routes/userLogin.js
@@ -6,25 +6,29 @@ const { redirectHome } = require('../middleware/redirect');
 
 const router = express.Router();
 
+/*
+    a request for login is sent
+        username & password is compared to Users database
+        a userID is assigned to the session cookie
+    if the user is not present in database, client is routed to /register
+*/
 
 router.get('/',  (req, res) => {
 res.json({ sesh: req.session });
 })
 
+
 router.post('/', redirectHome, async (req, res) => {
     const { username, password: plainTextPassword } = req.body;
-    // console.log(req.session);
     try {
         const user = await User.findOne({ username: `${username}`});
-        // console.log(user._id.toString());
         if(user) {
             if(await bcrypt.compare(plainTextPassword, user.password)) {
                 req.session.userID = user._id.toString();
-                // console.log(req.session.userID);
                 return res.status(200).json({message: "Login Successful"});
             }
         } else {
-            res.redirect('/login');
+            res.redirect('/register');
         }
     } catch (error) {
         throw error;
